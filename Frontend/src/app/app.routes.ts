da713c29c93cd92aba8f9705f36c95674bce80c1
Mauro Peralta <peraltamauro2001@gmail.com>
2026-02-09 20:01:44 -0300

    Modificacion de frontend parcial

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        loadComponent: () => import('./features/home/pages/home-page/home-page').then(m => m.HomePage)
+    },
+    {
+        path:'**',
+        loadComponent: () => import('./features/home/pages/home-page/home-page').then(m => m.HomePage)
+    }
+];
